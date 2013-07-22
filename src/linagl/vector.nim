3df683454e116e72da3ce406852f6556ad7a559f
Isak Andersson <BitPuffin@lavabit.com>
2013-07-22 22:21:07 +0200

    Work around generics not being able to accept values

--- src/linagl/vector.nim
+++ src/linagl/vector.nim
@@ -1,17 +1,17 @@
 ## Author: Isak Andersson
 
 type
-  TVector*[T, D: int] = object ## T = Type, D = Dimension
-    elements: array[1..D, T]
-  TVec2* = TVector[float, 2]
-  TVec3* = TVector[float, 3]
-  TVec4* = TVector[float, 4]
-  TVec2d* = TVector[float64, 2]
-  TVec3d* = TVector[float64, 3]
-  TVec4d* = TVector[float64, 4]
-  TVec2i* = TVector[int, 2]
-  TVec3i* = TVector[int, 3]
-  TVec4i* = TVector[int, 4]
+  TVector*[T, I:range] = object ## T = Type, I = Indices - will possibly later be changed to Dimension (number)
+    elements: array[I, T]
+  TVec2* = TVector[float, range[1..2]]
+  TVec3* = TVector[float, range[1..3]]
+  TVec4* = TVector[float, range[1..4]]
+  TVec2d* = TVector[float64, range[1..2]]
+  TVec3d* = TVector[float64, range[1..3]]
+  TVec4d* = TVector[float64, range[1..4]]
+  TVec2i* = TVector[int, range[1..2]]
+  TVec3i* = TVector[int, range[1..3]]
+  TVec4i* = TVector[int, range[1..4]]
 
 proc initVector*[T, D](elements: varargs[T]): TVector[T, D] =
   # TODO: Implement this shit
