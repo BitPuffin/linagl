da73e738795ac4a00e7832adaf22e5b610de7c68
Isak Andersson <BitPuffin@lavabit.com>
2013-07-23 11:41:11 +0200

    Add vector addition and subtraction

--- src/linagl/vector.nim
+++ src/linagl/vector.nim
@@ -12,3 +12,17 @@ type
   TVec3i* = TVector[int, range[0..2]]
   TVec4i* = TVector[int, range[0..3]]
 
+proc `+`*[T, I](a: TVector[T, I]): TVector[T, I] = 
+  result = a
+
+proc `+`*[T, I](a, b: TVector[T, I]): TVector[T, I] =
+  for i in low(a)..high(a):
+    result[i] = a[i] + b[i]
+
+proc `-`*[T, I](a: TVector[T, I]): TVector[T, I] =
+  for i in low(a)..high(a):
+    result[i] = -a[i]
+
+proc `-`*[T, I](a, b: TVector[T, I]): TVector[T, I] =
+  result = a + (-b)
+
