da65a584d91ac941ea55c606ea0862c0ed013a60
Isak Andersson <BitPuffin@lavabit.com>
2013-07-23 11:20:24 +0200

    Make TVector an array, much more simple to use

--- src/linagl/vector.nim
+++ src/linagl/vector.nim
@@ -1,18 +1,14 @@
 ## Author: Isak Andersson
 
 type
-  TVector*[T, I:range] = object ## T = Type, I = Indices - will possibly later be changed to Dimension (number)
-    elements: array[I, T]
-  TVec2* = TVector[float32, range[1..2]]
-  TVec3* = TVector[float32, range[1..3]]
-  TVec4* = TVector[float32, range[1..4]]
-  TVec2d* = TVector[float64, range[1..2]]
-  TVec3d* = TVector[float64, range[1..3]]
-  TVec4d* = TVector[float64, range[1..4]]
-  TVec2i* = TVector[int, range[1..2]]
-  TVec3i* = TVector[int, range[1..3]]
-  TVec4i* = TVector[int, range[1..4]]
+  TVector*[T; I:range] = array[I, T] ## T = Type, I = Indices - will possibly later be changed to Dimension (number)
+  TVec2* = TVector[float32, range[0..1]]
+  TVec3* = TVector[float32, range[0..2]]
+  TVec4* = TVector[float32, range[0..3]]
+  TVec2d* = TVector[float64, range[0..1]]
+  TVec3d* = TVector[float64, range[0..2]]
+  TVec4d* = TVector[float64, range[0..3]]
+  TVec2i* = TVector[int, range[0..1]]
+  TVec3i* = TVector[int, range[0..2]]
+  TVec4i* = TVector[int, range[0..3]]
 
-proc initVector*[T, D](elements: varargs[T]): TVector[T, D] =
-  when len(elements) != len(result.elements):
-    {.fatal: "Too many or too little parameters to match the vector dimension".}
