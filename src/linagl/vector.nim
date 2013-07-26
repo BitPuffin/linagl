4135bc9533a2f8ba0a242fe4663b09f0a162e3f2
EXetoC <exetoc@gmail.com>
2013-07-27 01:09:54 +0200

    Revert some stuff.

--- src/linagl/vector.nim
+++ src/linagl/vector.nim
@@ -1,11 +1,6 @@
 ## Author: Isak Andersson
 
-from math import
-  pow,
-  sqrt
-
-from strutils import
-  contains
+from math import pow, sqrt
 
 type
   TVector*[T; I:range] = array[I, T] ## T = Type, I = Indices - will possibly later be changed to Dimension (number)
@@ -21,7 +16,7 @@ type
 
 # This cannot yet take a TVector.
 proc `==`*[T:range, U](a, b: array[T, U]): bool =
-  for i in a.low..a.high:
+  for i in low(a)..high(a):
     if a[i] != b[i]:
       return false
 
@@ -29,7 +24,7 @@ proc `==`*[T:range, U](a, b: array[T, U]): bool =
 
 # Ditto.
 proc `~=`*[T:range, U](a, b: array[T, U]): bool =
-  for i in a.low..a.high:
+  for i in low(a)..high(a):
     if a[i] + b[i] > 0.000001:
       return false
 
@@ -39,25 +34,25 @@ template `+`*[T, I](a: TVector[T, I]): TVector[T, I] =
   a
 
 proc `+`*[T, I](a, b: TVector[T, I]): TVector[T, I] =
-  for i in a.low..a.high:
+  for i in low(a)..high(a):
     result[i] = a[i] + b[i]
 
 template add*[T, I](a, b: TVector[T, I]): TVector[T, I] =
   a + b
 
 proc `-`*[T, I](a: TVector[T, I]): TVector[T, I] =
-  for i in a.low..a.high:
+  for i in low(a)..high(a):
     result[i] = -a[i]
 
 proc `-`*[T, I](a, b: TVector[T, I]): TVector[T, I] =
-  for i in a.low..a.high:
+  for i in low(a)..high(a):
     result[i] = a[i] - b[i]
 
 template sub*[T, I](a, b: TVector[T, I]): TVector[T, I] =
   a - b
 
 proc mag*[T, I](a: TVector[T, I]): float =
-  for i in a.low..a.high:
+  for i in low(a)..high(a):
     when T is int:
       result += pow(toFloat(a[i]), 2.0)
     elif T is float:
@@ -67,14 +62,14 @@ proc mag*[T, I](a: TVector[T, I]): float =
   result = sqrt(result)
 
 proc dot*[T, I](a, b: TVector[T, I]): T =
-  for i in a.low..a.high:
+  for i in low(b)..high(b):
     result += a[i] * b[i]
 
 template `*.`*[T, I](a, b: TVector[T, I]): T =
   a.dot(b)
 
 proc cross*[T, I](a, b: TVector[T, I]): TVector[T, I] =
-  when high(a) - low(a) + 1 != 3:
+  when len(a) != 3:
     {.fatal: "Cross product only works with 3 dimensional vectors".}
   result = [
     a[1] * b[2] - b[1] * a[2],
@@ -88,7 +83,7 @@ proc dist*[T, I](a, b: TVector[T, I]): float =
   result = (a - b).mag
 
 proc `*`*[T, I](a: T; b: TVector[T, I]): TVector[T, I] =
-  for i in b.low..b.high:
+  for i in low(b)..high(b):
     result[i] = a * b[i]
 
 template `*`*[T, I](a: TVector[T, I]; b: T): TVector[T, I] =
@@ -97,7 +92,7 @@ template `*`*[T, I](a: TVector[T, I]; b: T): TVector[T, I] =
 # Kind of sucks for ints currently, perhaps convert an int vector to float in this case?
 proc normalize*[T, I](a: TVector[T, I]): TVector[T, I] =
   let m = mag(a)
-  for i in low.a..high.a:
+  for i in low(a)..high(a):
     result[i] = a[i] / m
 
 const
@@ -128,6 +123,8 @@ proc swizzleImpl[T, U](str: string): array[T, U] {.compileTime.} =
     else:
       # I don't know how to fail here. Pragmas are evaluated independently of the code flow.
 
+from strutils import contains
+
 template `{}`*[T, I](vec: TVector[T, I], str: string{lit}): expr =
   when str.contains({char(0)..char(255)} - swizzleChars):
     {.fatal: "Invalid characters for swizzling".}
@@ -145,7 +142,8 @@ template swizzle*[T, I](a: TVector[T, I], str: string{lit}): expr =
   a{str}
 
 proc `$`*[T, I](a: TVector[T, I]): string =
-  result = "["
+  result = ""
+  result &= "["
   var h = high(a)
   for i in low(a)..h:
     result &= $a[i]
@@ -157,14 +155,14 @@ template toString*[T, I](a: TVector[T, I]) =
   $a
 
 when isMainModule:
-  var v3 = [1, 3, 37]
-  assert v3{"rgbbg"} == [1, 3, 37, 37, 3]
-  assert v3{"rgbbg"} == v3.swizzle("rgbbg")
-  assert v3{"rrbbggr"} == [1, 1, 37, 37, 3, 3, 1]
+  var vec3: TVec3i = [1, 3, 37]
+  assert vec3{"rgbbg"} == [1, 3, 37, 37, 3]
+  assert vec3{"rgbbg"} == vec3.swizzle("rgbbg")
+  assert vec3{"rrbbggr"} == [1, 1, 37, 37, 3, 3, 1]
 
-  var v4 = [1, 3, 37, 4]
-  assert v4{"rgbbg"} == [1, 3, 37, 37, 3]
-  assert v4{"rrbaggr"} == [1, 1, 37, 4, 3, 3, 1]
+  var vec4: TVec4i = [1, 3, 37, 4]
+  assert vec4{"rgbbg"} == [1, 3, 37, 37, 3]
+  assert vec4{"rrbaggr"} == [1, 1, 37, 4, 3, 3, 1]
 
-  assert($v3 == "[1, 3, 37]")
-  assert($v4 == "[1, 3, 37, 4]")
+  assert($vec3 == "[1, 3, 37]")
+  assert($vec4 == "[1, 3, 37, 4]")
