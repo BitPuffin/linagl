c6809354cf21c3050c51eab1351adc1c78ff647e
Isak Andersson <BitPuffin@lavabit.com>
2013-07-26 02:07:14 +0200

    we can now finally have the cross product as it should be

--- src/linagl/vector.nim
+++ src/linagl/vector.nim
@@ -43,21 +43,12 @@ proc dot*[T, I](a, b: TVector[T, I]): T =
     result += a[i] * b[i]
 
 proc cross*[T, I](a, b: TVector[T, I]): TVector[T, I] =
-  # when len(a) != 3:
-  #  {.fatal: "Cross product only works with 3 dimensional vectors".}
-  # Uncomment this when Araq fixes !=
+  when len(a) != 3:
+    {.fatal: "Cross product only works with 3 dimensional vectors".}
   result =
     [a[1] * b[2] - b[1] * a[2],
      a[2] * b[0] - b[2] * a[0],
      a[0] * b[1] - b[0] * a[1]]
-
-# var a, b: TVec3i
-# a = [1, 2, 3]
-# b = [1, 3, 2]
-# var c = cross(a, b)
-# var d = cross(b, a]
-
-# echo("c: [", c[0], " ", c[1], " ", c[2], "]")
 
 proc dist*[T, I](a, b: TVector[T, I]): float =
   result = (a - b).mag
